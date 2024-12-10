93a29a92df44401f83634be6795b1aefec875c40
GAzaCSUN <grigor.azakian.22@my.csun.edu>
2024-12-09 20:52:25 -0800

    Sequential matrix multiplier

--- matrixmulti/src/main/java/com/matrixmulti/App.java
+++ matrixmulti/src/main/java/com/matrixmulti/App.java
@@ -1,23 +1,40 @@
 package com.matrixmulti;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
 public final class App {
-    public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "Matrix Multiplication");
-        job.setJarByClass(App.class);
-        job.setMapperClass(MatrixMapper.class);
-        job.setReducerClass(MatrixReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-        FileInputFormat.addInputPath(job, new Path(args[0])); 
-        FileOutputFormat.setOutputPath(job, new Path(args[1])); 
-        job.waitForCompletion(true);
+
+    public static void main(String[] args) {
+        int[][] A = { {5, 2, 3},
+                      {8, 3, 32},
+                      {13, 5, 12},
+                      {2, 8, 3}};
+
+        int[][] B = { {7, 6, 21, 6},
+                      {4, 4, 1, 5},
+                      {66, 7, 2, 4}};   
+                      
+        multiplyMatrix(A, B);
+    }    
+
+    static void multiplyMatrix(int[][] A, int[][] B) {
+        if (A[0].length != B.length) {
+            System.out.println("Incompatible matricies");
+        }
+
+        int C[][] = new int[A.length][B[0].length];
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B[0].length; j++) {
+                for (int k = 0; k < B.length; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < C.length; i++) {
+            for (int j = 0; j < C[0].length; j++) {
+                System.out.print(C[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
