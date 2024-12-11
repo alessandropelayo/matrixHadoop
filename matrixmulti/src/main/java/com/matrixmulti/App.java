5f49e0d78f256dbba7804fb84578da9240f65cc7
GAzaCSUN <grigor.azakian.22@my.csun.edu>
2024-12-11 10:47:12 -0800

    Implement file reading, and simple test for large matrix

--- matrixmulti/src/main/java/com/matrixmulti/App.java
+++ matrixmulti/src/main/java/com/matrixmulti/App.java
@@ -1,8 +1,15 @@
 package com.matrixmulti;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public final class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        /*
         int[][] A = { {5, 2, 3},
                       {8, 3, 32},
                       {13, 5, 12},
@@ -11,11 +18,20 @@ public final class App {
         int[][] B = { {7, 6, 21, 6},
                       {4, 4, 1, 5},
                       {66, 7, 2, 4}};   
-                      
-        multiplyMatrix(A, B);
+        */
+
+        List<int[][]> matrices = readMatrices("100.in");
+
+        long startTime = System.currentTimeMillis();
+        int[][] result = multiplyMatrix(matrices.get(0), matrices.get(1));
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+
+        printMatrix(result);
+        System.out.println("Matrix multiplication execution time: " + duration + " ms.");
     }    
 
-    static void multiplyMatrix(int[][] A, int[][] B) {
+    public static int[][] multiplyMatrix(int[][] A, int[][] B) {
         if (A[0].length != B.length) {
             System.out.println("Incompatible matricies");
         }
@@ -30,11 +46,52 @@ public final class App {
             }
         }
 
-        for (int i = 0; i < C.length; i++) {
-            for (int j = 0; j < C[0].length; j++) {
-                System.out.print(C[i][j] + " ");
+        return C;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
+    }
+
+    public static List<int[][]> readMatrices(String filepath) throws IOException {
+        List<int[][]> matrices = new ArrayList<>();
+        List<int[]> matrix = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (!matrix.isEmpty() && line.trim().isEmpty()) {
+                    int[][] temp = new int[matrix.size()][];
+                    for (int i = 0; i < matrix.size(); i++) {
+                        temp[i] = matrix.get(i);
+                    }
+                    matrices.add(temp);
+                    matrix.clear();
+                }
+                else {
+                    String[] rowLine = line.trim().split("\\s+");
+                    int[] row = new int[rowLine.length];
+                    for (int i = 0; i < rowLine.length; i++) {
+                        row[i] = Integer.parseInt(rowLine[i]);
+                    }
+                    matrix.add(row);
+                }
+            }
+        }
+
+        if (!matrix.isEmpty()) {
+            int[][] temp = new int[matrix.size()][];
+            for (int i = 0; i < matrix.size(); i++) {
+                temp[i] = matrix.get(i);
+            }
+            matrices.add(temp);
+        }
+
+        return matrices;
     }
 }
